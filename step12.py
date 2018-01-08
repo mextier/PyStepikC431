0a326baa4b8f58349ea951fb74cefda2240fcdc8
mextier <mextier@gmail.com>
2018-01-08 18:22:21 +0600

    12

---
+++ step12.py
@@ -0,0 +1,12 @@
+
+
+n = int(input())
+while n!=1:
+    print(str(n),'',end='')
+    n = n//2 if n%2==0 else 1+3*n
+print(n)
+
+l = [int(input())]
+while l[-1:][0]!=1:
+    l.append(l[-1]/2 if l[-1]%2==0 else 1+3*l[-1])
+print(" ".join(str(c) for c in list(map(int,l))))
